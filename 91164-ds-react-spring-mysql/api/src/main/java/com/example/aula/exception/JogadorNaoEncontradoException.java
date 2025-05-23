558519d4b90e57c50364037377d98198247adb3f
Natan William de Santana Silva <167127767+nxtannnn1@users.noreply.github.com>
2025-05-23 19:07:09 -0300

    Delete 91164-ds-react-spring-mysql directory

--- 91164-ds-react-spring-mysql/api/src/main/java/com/example/aula/exception/JogadorNaoEncontradoException.java
+++
@@ -1,8 +0,0 @@
-package com.example.aula.exception;
-
-public class JogadorNaoEncontradoException extends RuntimeException{
-
-    public JogadorNaoEncontradoException (String mensagem){
-        super(mensagem);
-    }
-}
